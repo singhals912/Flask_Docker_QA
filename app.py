145e3ceee836eac500322ac77ef9b80467374414
Sachin Singhal <singhals912@gmail.com>
2021-06-03 17:21:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,13 +56,6 @@ db_connect_string = " ".join([
       dbname
     ])
 
-# --------------#
-#  VARIABLES   #
-# --------------#
-
-# Create my flask app
-app = Flask(__name__)
-
 # Create a variable that will hold our models in memory
 models = {}
 
@@ -71,170 +64,178 @@ db = 'answers.db'
 
 
 # --------------#
-#    ROUTES    #
+#  VARIABLES   #
 # --------------#
+def create_app():
+      
+      # Create my flask app
+      app = Flask(__name__)
 
-# Define a handler for the / path, which
-# returns a message and allows Cloud Run to
-# health check the API.
-@app.route("/")
-def hello_world():
-    return "<p>The question answering API is healthy!</p>"
+      # --------------#
+      #    ROUTES    #
+      # --------------#
 
-
-# Define a handler for the /answer path, which
-# processes a JSON payload with a question and
-# context and returns an answer using a Hugging
-# Face model.
-@app.route("/answer", methods=['POST'])
-def answer():
-    # Get the request body data
-    data = request.json
-
-    # Validate model name if given
-    if request.args.get('model') != None:
-        if not validate_model(request.args.get('model')):
-            return "Model not found", 400
-
-    # Answer the question
-    answer, model_name = answer_question(request.args.get('model'),
-                                         data['question'], data['context'])
-    timestamp = int(time.time())
-
-    # Insert our answer in the database
-    con = psycopg2.connect(db_connect_string)
-    cur = con.cursor()
-    sql = "INSERT INTO answers VALUES ('{question}','{context}','{model}','{answer}',{timestamp})"
-    cur.execute(sql.format(
-        question=data['question'].replace("'", "''"),
-        context=data['context'].replace("'", "''"),
-        model=model_name,
-        answer=answer,
-        timestamp=str(timestamp)))
-    con.commit()
-    con.close()
-
-    # Create the response body.
-    out = {
-        "question": data['question'],
-        "context": data['context'],
-        "answer": answer,
-        "model": model_name,
-        "timestamp": timestamp
-    }
-
-    return jsonify(out)
+      # Define a handler for the / path, which
+      # returns a message and allows Cloud Run to
+      # health check the API.
+      @app.route("/")
+      def hello_world():
+          return "<p>The question answering API is healthy!</p>"
 
 
-# List historical answers from the database.
-@app.route("/answer", methods=['GET'])
-def list_answer():
-    # Validate timestamps
-    if request.args.get('start') == None or request.args.get('end') == None:
-        return "Query timestamps not provided", 400
+      # Define a handler for the /answer path, which
+      # processes a JSON payload with a question and
+      # context and returns an answer using a Hugging
+      # Face model.
+      @app.route("/answer", methods=['POST'])
+      def answer():
+          # Get the request body data
+          data = request.json
 
-    # Prep SQL query
-    if request.args.get('model') != None:
-        sql = "SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end} AND model == '{model}'"
-        sql_rev = sql.format(start=request.args.get('start'),
-                             end=request.args.get('end'), model=request.args.get('model'))
-    else:
-        sql = 'SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end}'
-        sql_rev = sql.format(start=request.args.get('start'), end=request.args.get('end'))
+          # Validate model name if given
+          if request.args.get('model') != None:
+              if not validate_model(request.args.get('model')):
+                  return "Model not found", 400
 
-    # Query the database
-    con = psycopg2.connect(db_connect_string)
-    cur = con.cursor()
-    out = []
-    for row in cur.execute(sql_rev):
-        out.append({
-            "question": row[0],
-            "context": row[1],
-            "answer": row[2],
-            "model": row[3],
-            "timestamp": row[4]
-        })
-    con.close()
+          # Answer the question
+          answer, model_name = answer_question(request.args.get('model'),
+                                               data['question'], data['context'])
+          timestamp = int(time.time())
 
-    return jsonify(out)
+          # Insert our answer in the database
+          con = psycopg2.connect(db_connect_string)
+          cur = con.cursor()
+          sql = "INSERT INTO answers VALUES ('{question}','{context}','{model}','{answer}',{timestamp})"
+          cur.execute(sql.format(
+              question=data['question'].replace("'", "''"),
+              context=data['context'].replace("'", "''"),
+              model=model_name,
+              answer=answer,
+              timestamp=str(timestamp)))
+          con.commit()
+          con.close()
 
+          # Create the response body.
+          out = {
+              "question": data['question'],
+              "context": data['context'],
+              "answer": answer,
+              "model": model_name,
+              "timestamp": timestamp
+          }
 
-# List models currently available for inference
-@app.route("/models", methods=['GET'])
-def list_model():
-    # Get the loaded models
-    models_loaded = []
-    for m in models['models']:
-        models_loaded.append({
-            'name': m['name'],
-            'tokenizer': m['tokenizer'],
-            'model': m['model']
-        })
-
-    return jsonify(models_loaded)
+          return jsonify(out)
 
 
-# Add a model to the models available for inference
-@app.route("/models", methods=['PUT'])
-def add_model():
-    # Get the request body data
-    data = request.json
+      # List historical answers from the database.
+      @app.route("/answer", methods=['GET'])
+      def list_answer():
+          # Validate timestamps
+          if request.args.get('start') == None or request.args.get('end') == None:
+              return "Query timestamps not provided", 400
 
-    # Load the provided model
-    if not validate_model(data['name']):
-        models_rev = []
-        for m in models['models']:
-            models_rev.append(m)
-        models_rev.append({
-            'name': data['name'],
-            'tokenizer': data['tokenizer'],
-            'model': data['model'],
-            'pipeline': pipeline('question-answering',
-                                 model=data['model'],
-                                 tokenizer=data['tokenizer'])
-        })
-        models['models'] = models_rev
+          # Prep SQL query
+          if request.args.get('model') != None:
+              sql = "SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end} AND model == '{model}'"
+              sql_rev = sql.format(start=request.args.get('start'),
+                                   end=request.args.get('end'), model=request.args.get('model'))
+          else:
+              sql = 'SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end}'
+              sql_rev = sql.format(start=request.args.get('start'), end=request.args.get('end'))
 
-    # Get the loaded models
-    models_loaded = []
-    for m in models['models']:
-        models_loaded.append({
-            'name': m['name'],
-            'tokenizer': m['tokenizer'],
-            'model': m['model']
-        })
+          # Query the database
+          con = psycopg2.connect(db_connect_string)
+          cur = con.cursor()
+          out = []
+          for row in cur.execute(sql_rev):
+              out.append({
+                  "question": row[0],
+                  "context": row[1],
+                  "answer": row[2],
+                  "model": row[3],
+                  "timestamp": row[4]
+              })
+          con.close()
 
-    return jsonify(models_loaded)
+          return jsonify(out)
 
 
-# Delete a model from the models available for inference
-@app.route("/models", methods=['DELETE'])
-def delete_model():
-    # Validate model name if given
-    if request.args.get('model') == None:
-        return "Model name not provided in query string", 400
+      # List models currently available for inference
+      @app.route("/models", methods=['GET'])
+      def list_model():
+          # Get the loaded models
+          models_loaded = []
+          for m in models['models']:
+              models_loaded.append({
+                  'name': m['name'],
+                  'tokenizer': m['tokenizer'],
+                  'model': m['model']
+              })
 
-    # Error if trying to delete default model
-    if request.args.get('model') == models['default']:
-        return "Can't delete default model", 400
+          return jsonify(models_loaded)
 
-    # Load the provided model
-    models_rev = []
-    for m in models['models']:
-        if m['name'] != request.args.get('model'):
-            models_rev.append(m)
-    models['models'] = models_rev
 
-    # Get the loaded models
-    models_loaded = []
-    for m in models['models']:
-        models_loaded.append({
-            'name': m['name'],
-            'tokenizer': m['tokenizer'],
-            'model': m['model']
-        })
+      # Add a model to the models available for inference
+      @app.route("/models", methods=['PUT'])
+      def add_model():
+          # Get the request body data
+          data = request.json
 
-    return jsonify(models_loaded)
+          # Load the provided model
+          if not validate_model(data['name']):
+              models_rev = []
+              for m in models['models']:
+                  models_rev.append(m)
+              models_rev.append({
+                  'name': data['name'],
+                  'tokenizer': data['tokenizer'],
+                  'model': data['model'],
+                  'pipeline': pipeline('question-answering',
+                                       model=data['model'],
+                                       tokenizer=data['tokenizer'])
+              })
+              models['models'] = models_rev
+
+          # Get the loaded models
+          models_loaded = []
+          for m in models['models']:
+              models_loaded.append({
+                  'name': m['name'],
+                  'tokenizer': m['tokenizer'],
+                  'model': m['model']
+              })
+
+          return jsonify(models_loaded)
+
+
+      # Delete a model from the models available for inference
+      @app.route("/models", methods=['DELETE'])
+      def delete_model():
+          # Validate model name if given
+          if request.args.get('model') == None:
+              return "Model name not provided in query string", 400
+
+          # Error if trying to delete default model
+          if request.args.get('model') == models['default']:
+              return "Can't delete default model", 400
+
+          # Load the provided model
+          models_rev = []
+          for m in models['models']:
+              if m['name'] != request.args.get('model'):
+                  models_rev.append(m)
+          models['models'] = models_rev
+
+          # Get the loaded models
+          models_loaded = []
+          for m in models['models']:
+              models_loaded.append({
+                  'name': m['name'],
+                  'tokenizer': m['tokenizer'],
+                  'model': m['model']
+              })
+
+          return jsonify(models_loaded)
 
 
 # --------------#
